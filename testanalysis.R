19a9a5dba2d7f6bb63db8b69380b9aaf41136e1b
Toni Mizerek <toni.mizerek@mq.edu.au>
2015-06-05 10:26:29 +1000

    a function

---
+++ testanalysis.R
@@ -0,0 +1,13 @@
+change_CTDB <- function(x) {
+  if (length(x) > 1) {      # Does a species by trait combination have more than 1 value?
+    x <- type.convert(x, as.is=TRUE)
+    if (is.character(x)) {
+      return(x[1])          # If values are strings (characters), then return the first value
+    } else {
+      return(as.character(max(x)))  # If values are numbers, then return the max (converted back to character). for depth range, we're interested in max.
+    }
+  } else {
+    return(x) # If a species by trait combination has 1 value, then just return that value 
+  }
+}
+
